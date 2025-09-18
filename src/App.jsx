2dfb1460838210829eb1fcea0a62ae244c6c1ea0
reyGon2005 <reycasillas9@gmail.com>
2025-09-18 11:55:41 -0600

    logica y app funcional solo falta decorar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react'
 import './App.css'
 import TrackList from './TrackList'
-
 import SearchBar from './SearchBar'
 
-function App(prop) {
-  
+function App() {
   const [lsitaPokemon, setListaPokemon] = useState([]);
 
-  function addPokemon(){
+  function addPokemon(pokemon) {
     setListaPokemon([...lsitaPokemon, pokemon]);
+  }
+
+  function clearList() {
+    alert("lista vacia")
+    setListaPokemon([]);
   }
 
   return (
     <>
       <SearchBar onAdd={addPokemon}/>
-      <TrackList listaPokemon={lsitaPokemon}/>
+      <TrackList listaPokemon={lsitaPokemon} onClear={clearList}/>
     </>
   )
 }
 
-export default App
+export default App;
--- src/TrackList.jsx
+++ src/TrackList.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 
-function TrackList(prop){
-    return (
-        <>
-            <h2>Lista Pokemones</h2>
-            <ul>
-                {prop.listaPokemon.map((poke)=>(
-                    <li key={poke.id}>
-                        {poke.name}
-                        <img src={poke.sprites.front_default} alt={poke.name} />
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+function TrackList({ listaPokemon, onClear }) {
+  return (
+    <>
+      <h2>Lista Pokemones</h2>
+      <button onClick={onClear}>Limpiar lista</button>
+      <ul>
+        {listaPokemon.map((poke) => (
+          <li key={poke.id}>
+            {poke.name}
+            <img src={poke.sprites.front_default} alt={poke.name} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default TrackList;
